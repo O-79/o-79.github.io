243869770b97285207f2c642e3e564f71705843b
O-79 <adiguptaxd@gmail.com>
2024-08-12 21:31:41 -0400

    added theme toggle icon resources

---
+++ theme.js
@@ -0,0 +1,15 @@
+const theme_toggle = document.getElementById('theme-toggle');
+const theme_icon = document.getElementById('theme-icon');
+const GIF_COW = document.getElementById('gif-cow');
+const body = document.body;
+
+theme_toggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        theme_icon.src = 'resources/ICO-Sun.png';
+		GIF_COW.src = 'resources/GIF-Cow-ALT.gif';
+    } else {
+        theme_icon.src = 'resources/ICO-Moon.png';
+		GIF_COW.src = 'resources/GIF-Cow.gif';
+    }
+});
